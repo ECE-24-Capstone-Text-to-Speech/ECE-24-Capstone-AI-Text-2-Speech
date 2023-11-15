44fb35395a5e629b085a5bd939d525a368f580ea
Sachin Nair <sachin.nair@utexas.edu>
2023-11-15 16:42:14 -0600

    fixing some endpoints

--- Backend/db.py
+++ Backend/db.py
@@ -5,32 +5,13 @@ import certifi
 uri = "mongodb+srv://seniordesignusername:123password123@seniordesigncluster.zhtgtmr.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
-'''
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-'''
-
-import pymongo
-from flask import Flask
-import os
-from flask import Flask
 from dotenv import load_dotenv
-from fastapi import APIRouter
-import pymongo
-import certifi
-
-
 ca = certifi.where()
 client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=ca)
 db = client["SeniorDesignProject"]
 users = db["users"]
 #Example of inserting users into database
-def register():
-
+def register(): 
         user = "testUser"
         user_found = users.find_one({"username": user})
         if user_found:
--- Backend/routers/users.py
+++ Backend/routers/users.py
@@ -1,20 +1,19 @@
 # TODO: all functions here have to fetch data from MongoDB
-import os
-from flask import Flask, session, response
-from dotenv import load_dotenv
-from fastapi import APIRouter, request
-import pymongo
+#from dotenv import load_dotenv
+from fastapi import APIRouter, Response, Request, responses
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import certifi
 
 
 router = APIRouter()
 
 
-load_dotenv()
-app= Flask(__name__)
-client = pymongo.MongoClient(os.getenv(uri))
+#load_dotenv()
+uri = "mongodb+srv://seniordesignusername:123password123@seniordesigncluster.zhtgtmr.mongodb.net/?retryWrites=true&w=majority"
+ca = certifi.where()
+client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=ca)
 db = client["SeniorDesignProject"]
-
-#crate collection
 users = db["users"]
 
 @router.get("/users/", tags=["users"])
@@ -26,37 +25,39 @@ async def read_users():
 async def read_user_me():
     return {"username": "fakecurrentuser"}
 
-
+'''
 @router.get("/users/{username}", tags=["users"])
 async def read_user(username: str):
     return {"username": username}
+'''
 
 @router.get('/users/register', tags=['users'])
-async def register():
-    if request.tag == "users":
-        message = ""
-        json = request.get_json()
-        user = json["username"]
-        user_found = users.find_one({"username": user})
-        if user_found:
-            message = 'username already exists'
-            return message
+async def register(response : Response):
+    message = ""
+    json = response.get_json()
+    user = json.get("username", None)
+    if not user:
+        message = "username cannot be empty"
+        return message
+    user_found = users.find_one({"username": user})
+    if user_found:
+        message = 'username already exists'
+        return message
 
-        ditem = {
-            "username" : json["username"],
-            "password" : json["password"]
-        }
-        users.insert_one(ditem)
-        
-        return ditem["username"]
+    newUser = {
+        "username" : json.get("username"),
+        "password" : json.get("password")
+    }
+    users.insert_one(newUser)
+    
+    return newUser["username"]
 
 
-@app.route("/users/login", methods=["POST"])
-async def login():
-    json = request.get_json(force=True)
-    print(json)
-    user = json['userid']
-    password = json['password']
+@router.get("/users/login", tags=["users"])
+async def login(response : Response):
+    
+    user = response.get("username")
+    password = response.get("password")
     user_found = users.find_one({"userid": user})
     if user_found is not None:
         # user exists
@@ -75,15 +76,30 @@ async def login():
         return message
 
 
-@app.route("/users/logout")
-async def logout_user():
+@router.get("/users/logout", tags=["users"])
+async def logout_user(response : Response):
     result = False
     message = "Error: No user in session to logout"
 
     # if the session currently has a user, remove the user\
-    if session.keys() == "loggedInSession":
-        response.set_cookie(key="loggedOutSession", value = "")
+    if "loggedInSession" in response.cookies.keys():
+        response.delete_cookie("loggedInSession")
         result = True
         message = "User successfully logged out"
 
     return message
+
+
+
+@router.get("/users/testLogin", tags=["users"])
+async def testLoginCookies(request: Request):
+    response = responses.JSONResponse(content={"message": "hello"})
+    print(request)
+
+    # Use `Response` to set a cookie in the request
+    response.set_cookie(key="loggedInSession", value="123123123 cookie")
+
+    message = request.cookies.get("loggedInSession")
+    return {"Message": message}
+
+
