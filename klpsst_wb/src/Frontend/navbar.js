c1865762e56afc566c0259bf1e023cbac0181e0f
Purva Kantawala <p.kantawala0@utexas.edu>
2023-11-15 17:05:09 -0600

    Converted to MaterialUI. Improved visuals. Added dark mode feature.

--- klpsst_wb/src/Frontend/navbar.js
+++ klpsst_wb/src/Frontend/navbar.js
@@ -1,27 +1,31 @@
-import React, {useState} from "react";
-// import styled from "styled-components";
+import React from "react";
 import "./navbar.css";
 import { Link, useLocation } from "react-router-dom";
-import { render } from "@testing-library/react";
 
-const KLPSST_Bar = ({}) => {
+const KLPSST_Bar = () => {
+  const page = useLocation();
+  const pages = ["Home", "Login"];
 
-    const page = useLocation(); //gets what pg ure on
-    const pages = ['Home', 'Login']
+  return (
+    <div id="test">
+      {pages.map((pageTitle) => (
+        <Link
+          key={pageTitle}
+          to={`/${pageTitle.toLowerCase()}`}
+          className={
+            page.pathname === `/${pageTitle.toLowerCase()}` ||
+            (pageTitle === "Home" && (page.pathname === "" || page.pathname === "/"))
+              ? "active"
+              : ""
+          }
+          style={{ paddingLeft: "10px", paddingRight: "10px" }} // Adjust the padding as needed
 
-    return (
-        <div id = "test">
-            {pages.map((pageTitle) => (
-                <Link
-                    key = {pageTitle}
-                    to={`/${pageTitle.toLowerCase()}`}
-                    className={page.pathname === `/${pageTitle.toLowerCase()}` ? 'active' : ''}
-                >
-                    {pageTitle}
-                </Link>
-            ))}
-        </div>
-    );
-}
+        >
+          {pageTitle}
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default KLPSST_Bar;
