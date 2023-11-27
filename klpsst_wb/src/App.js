009d4f0b62e930c78532fe9005890682546fbc68
--replace-all <tristanbecnel87@gmail.com>
2023-11-27 00:22:28 -0600

    frontend endpoints

--- klpsst_wb/src/App.js
+++ klpsst_wb/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
-import KLPSST_Page from './Frontend/klpsst.js';
+import { KLPSST_Page, S3UploadPage } from './Frontend/klpsst';
+
 
 function App() {
   return (
     <div className = "App">
-      <KLPSST_Page name = "hee" />          
+      <KLPSST_Page name = "hee" />  
+      <S3UploadPage name = "haa"/>        
     </div>
+    
   );
 }
 
