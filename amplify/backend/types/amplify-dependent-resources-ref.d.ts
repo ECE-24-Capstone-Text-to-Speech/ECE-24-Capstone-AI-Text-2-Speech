00a6412182cc4fe8017e92fde4fafaf47923c0aa
--replace-all <tristanbecnel87@gmail.com>
2023-11-26 18:18:32 -0600

    tortoisev2 works

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "klpsstapp3936b494": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "klpsst": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
