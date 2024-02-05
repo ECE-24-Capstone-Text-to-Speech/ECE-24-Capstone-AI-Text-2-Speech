d3d446b896e8f52118ebfff511df6ec31c550e6d
Sachin Nair <sachin.nair@utexas.edu>
2024-02-05 13:50:26 -0600

    NUKE

--- Backend/README.md
+++
@@ -1,2 +0,0 @@
-# Backend
-Download docker desktop. Then cd into `Backend` folder and run `bash ./start.sh` or `bash ./run.sh` to start server
--- Backend/internal/cookies.py
+++
@@ -1,8 +0,0 @@
-
-
-from fastapi import Request
-
-
-def getCurrUser(request: Request) -> str | None:
-    currUser = request.cookies.get("loggedInSession", None)
-    return currUser
