36d3784460a86eaf5c20280aab84fbc95be1dd96
Suha <suhaakhund@gmail.com>
2024-02-19 15:38:36 -0600

    logout button functionalities

--- klpsst_wb/src/Frontend/klpsst.js
+++ klpsst_wb/src/Frontend/klpsst.js
@@ -4,8 +4,9 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 // import styled from "styled-components";
 import "./klpsst.css";
-import { Navigate, redirect } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import KLPSSTLOGO from "./logo_image.png";
+import KLPSST_Login from "./login";
 // import { routeManager } from "../../routeManager";
 import { render } from "@testing-library/react";
 //const backendURL;
@@ -15,6 +16,11 @@ const KLPSST_Page = () => {
   const initialTheme = storedTheme ? JSON.parse(storedTheme) : "light";
   const [file1, setFile1] = useState("");
   const [file2, setFile2] = useState("");
+  const [message, setMessage] = useState("");
+
+  //redirection code
+  const navigate = useNavigate();
+  const [redirect, setRedirect] = useState(false);
 
   // Define dark mode theme
   const darkTheme = createTheme({
@@ -22,9 +28,6 @@ const KLPSST_Page = () => {
       mode: "dark",
     },
   });
-
-  // console.log("dsfyyus");
-  // console.log(localStorage.getItem("loggedIn"));
 
   // Define light mode theme
   const lightTheme = createTheme({
@@ -129,15 +132,67 @@ const KLPSST_Page = () => {
     } catch (error) {
       // Handle network errors
       console.error("Error uploading files:", error);
+      alert("In order to upload, please log in!");
     }
   };
+
+  const logOut = async (e) => {
+    e.preventDefault();
+
+    if (localStorage.getItem("loggedIn") === "true") {
+      try {
+        const response = await fetch("http://localhost:80/users/logout", {
+          method: "POST",
+          credentials: "include",
+        });
+
+        // console.log(response);
+
+        if (response.ok) {
+          const errorMessage = await response.json();
+          localStorage.setItem("username", "");
+          localStorage.setItem("password", "");
+
+          console.log(errorMessage);
+          if (errorMessage == "Error: No user in session to logout") {
+            console.log("Naur");
+            alert(`Logout unsucessful`);
+          } else if (errorMessage == "User successfully logged out") {
+            console.log("Yer");
+            localStorage.setItem("loggedIn", false);
+            setRedirect(true); //for redirection?
+            alert("User successfully logged out");
+          }
+        } else {
+          const errorMessage = await response.json();
+          setMessage(errorMessage.error || "Logout failed");
+          alert(errorMessage);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setMessage("An error occurred. Please try again later.");
+      }
+    } else {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    console.log(localStorage.getItem("loggedIn"));
+    return <Navigate to="/login" />;
+  }
 
   return (
     <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
       <div id="homepage" className={theme === "dark" ? "dark-mode" : ""}>
-        <Button onClick={toggleTheme} id="toggleButton">
-          {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
-        </Button>
+        <div id="buttons">
+          <Button onClick={toggleTheme} id="toggleButton">
+            {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
+          </Button>
+          <Button onClick={logOut} id="checkLogin">
+            {localStorage.getItem("loggedIn") === "true" ? "Logout" : "Login"}
+          </Button>
+        </div>
         <Typography variant="h1">KLPSST</Typography>
         <img
           src={KLPSSTLOGO}
