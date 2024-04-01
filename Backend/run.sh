7ec5ae410fee5e7338ec64622246ffb41ef3fc04
Sachin Nair <sachin.nair@utexas.edu>
2024-04-01 14:00:19 -0500

    run.sh

--- Backend/run.sh
+++ Backend/run.sh
@@ -5,11 +5,22 @@ cd $dir
 mkdir local
 
 echo "building image for fastapi backend"
-docker build -t my-fastapi-app .
+docker build . -t tts
 
 echo "starting fastapi backend service"
-docker run -v "$(pwd):/server" -p 80:80 my-fastapi-app
+#docker run -v "$(pwd):/server" -p 80:80 tts
+#SACHIN ONLY
+
+current_dir=$(pwd)
+
+docker run -p 80:80 \
+    -e "TORTOISE_MODELS_DIR=/models" \
+    -v "$current_dir/docker_data/models:/models" \
+    -v "$current_dir/docker_data/results:/results" \
+    -v "$current_dir/docker_data/.cache/huggingface:/root/.cache/huggingface" \
+    -v "$current_dir/docker_data/work:/work" \
+    -it tts
 
 echo "shutting down fastapi server"
-docker stop my-fastapi-app
-docker rm my-fastapi-app
+docker stop tts
+docker rm tts
