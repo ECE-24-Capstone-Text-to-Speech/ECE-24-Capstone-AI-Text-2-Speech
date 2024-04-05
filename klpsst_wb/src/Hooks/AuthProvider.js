1bf0ad454a39001a7220806dada538dc9ba0e240
Suha <suhaakhund@gmail.com>
2024-04-05 15:26:26 -0500

    working login + status check:

--- klpsst_wb/src/Hooks/AuthProvider.js
+++ klpsst_wb/src/Hooks/AuthProvider.js
@@ -15,9 +15,13 @@ const AuthProvider = ({ children }) => {
   const isAuth = async () => {
     console.log("Checking log in status...");
     try {
+      const token = sessionStorage.getItem("token");
+      console.log(token);
       fetch(process.env.REACT_APP_SERVER_ADDRESS + "/users/loginStatus", {
         method: "GET",
-        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       })
         .then((res) => {
           return res.json();
